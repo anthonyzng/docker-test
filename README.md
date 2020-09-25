9280793c80a45eebe8794a311bd521ca8a0f3d12
paniomlaw <lawtinloklhl@gmail.com>
2020-09-25 12:54:10 +0800

    committed by @jswildcards

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # docker-test
-test docker with node.js
+## How to Use it
+```bash
+$ git clone https://github.com/anthonyzng/docker-test.git
+$ cd docker-test
+$ docker build -t ant:1.0.0 .
+$ docker run -dp 8888:8888 --name ant ant:1.0.0
+```
+
+Then go to http://localhost:8888 and see the result.
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 var express = require('express');
 var app = express();
 //----------------------catch data tool
-const sp = require('superagent'); 
+const sp = require('superagent');
 const cheerio = require('cheerio');
 //--------------------------------------mongo db
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
 const url = "mongodb+srv://tung:29448953@cluster0.lfzgt.mongodb.net/test";
 //---------------------------------------------
 //-------------------------------Restful API
 app.get('/', (req, res) => {
     return res.send('Received a GET HTTP method');
-  });
+});
 
-  app.listen(8888, () => {
-      console.log(`Hello world app listening on port 8888!`);
+app.listen(8888, () => {
+    console.log(`Hello world app listening on port 8888!`);
 
     //   MongoClient.connect(url, function(err, db) {
     //     if (err) throw err;
@@ -25,26 +25,20 @@ app.get('/', (req, res) => {
     //       db.close();
     //     });
     //   });
-
-        
-    });
+});
 
 // data link1:https://www.openrice.com/zh/hongkong/restaurants?where=%E9%95%B7%E6%B2%99%E7%81%A3
 // data link2:https://www.openrice.com/zh/hongkong/restaurants?where=%E8%8D%94%E6%9E%9D%E8%A7%92
 
-function catch_data(){
+function catch_data() {
     const BASE_URL = "https://www.openrice.com/zh/hongkong/restaurants?where=%E9%95%B7%E6%B2%99%E7%81%A3";
 
-    async () => { 
+    async () => {
         let html = await sp.get(BASE_URL);
         let $ = cheerio.load(html.text);
         let RestList = [];
-        $('#or-route-poi-list-main ul li').each(function(){
+        $('#or-route-poi-list-main ul li').each(function () {
 
         });
-
-        
-
-        }
-
+    }
 }
